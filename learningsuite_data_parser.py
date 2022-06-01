c4d929925891e4a2256453ca58b481a6813e12a3
Zach227 <104393986+Zach227@users.noreply.github.com>
2022-06-01 16:34:04 -0600

    Update learningsuite_data_parser.py

--- learningsuite_data_parser.py
+++ learningsuite_data_parser.py
@@ -5,10 +5,20 @@ import numpy as np
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--directory", help="collects the module data for all files in directory")
+parser.add_argument(
+    "-md",
+    "--modulesdirectory",
+    help="collects the module data for all files in directory",
+)
 parser.add_argument("-f", "--file", help="collects the module data for a single file")
-parser.add_argument("-p", "--participants", help="collects the data for each participant")
-parser.add_argument("-v", "--verbose", action="store_true", help="print logging messages")
+parser.add_argument(
+    "-pd",
+    "--participantsdirectory",
+    help="collects the data for each participant from a directory",
+)
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="print logging messages"
+)
 args = parser.parse_args()
 
 # establish paths, make directory
@@ -31,6 +41,8 @@ def fix_file(file_name):
             newf.write(fixed)
         os.chdir(data_dir_path)
 
+
+# create data summaries for each module
 def get_module_info(file_name, data):
     float_hours = []
     for value in data.iloc[:, 7]:
@@ -59,20 +71,74 @@ def get_module_info(file_name, data):
     os.remove(temptxt_path)
     os.chdir(data_dir_path)
 
+
+# read module data and organize data by student
+def get_student_info(file_name, data):
+    if file_name[0:38] == "Student Answers for Computing Bootcamp":
+        module_name = file_name[39:-4].replace(" ", "")
+    elif file_name[-11:] == "_Module.csv":
+        module_name = file_name[:-11]
+    else:
+        module_name = file_name[:-4]
+    student_list = data.iloc[:, 0]
+    i = 0
+    for student in student_list:
+        if student not in student_modules:
+            student_modules[student] = []
+            student_hours[student] = []
+        student_data = data.iloc[i, :]
+        if student_data[6] == "A":
+            student_modules[student].append(module_name)
+        student_hours[student].append(student_data[7])
+        i = i + 1
+
+
+# create a textfile for each student from student data
+def create_student_summaries():
+    os.chdir(participantSummaries_dir_path)
+    for student in student_modules:
+        new_file_name = f'{student.replace(" ", "")}_Results.txt'
+        total_time = 0
+        for value in student_hours[student]:
+            if not pd.isnull(value):
+                total_time = total_time + value
+        if total_time != 0:
+            with open(new_file_name, "w") as out:
+                out.write(
+                    f"{student} completed {len(student_modules[student])} modules:\n"
+                )
+                for mod in student_modules[student]:
+                    out.write(f"    {mod}\n")
+                out.write(f"\nTotal hours spent: {total_time}")
+
+
+# main()
 if args.file:
     if not os.path.isdir(moduleSummaries_dir_path):
         os.mkdir(moduleSummaries_dir_path)
     fix_file(args.file)
     data = pd.read_csv("temp.txt")
     get_module_info(args.file, data)
-elif args.directory:
+elif args.modulesdirectory:
     if not os.path.isdir(moduleSummaries_dir_path):
         os.mkdir(moduleSummaries_dir_path)
-    data_dir_path = os.path.join(ran_in_path, args.directory)
+    data_dir_path = os.path.join(ran_in_path, args.modulesdirectory)
     for file_name in os.listdir(data_dir_path):
         os.chdir(data_dir_path)
         fix_file(file_name)
         data = pd.read_csv(temptxt_path)
         get_module_info(file_name, data)
-elif args.participants:
-    pass
+elif args.participantsdirectory:
+    if not os.path.isdir(participantSummaries_dir_path):
+        os.mkdir(participantSummaries_dir_path)
+    data_dir_path = os.path.join(ran_in_path, args.participantsdirectory)
+    student_modules = {}
+    student_hours = {}
+    for file_name in os.listdir(data_dir_path):
+        os.chdir(data_dir_path)
+        fix_file(file_name)
+        data = pd.read_csv(temptxt_path)
+        get_student_info(file_name, data)
+    create_student_summaries()
+else:
+    print("No options were selected, use -h option for help")
